83b983d9b893cd9d420981dd72628e4cafd3ba00
DuongLT <duonglt.work@gmail.com>
2023-04-16 18:47:22 +0700

    config email, notify user comment product detail

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,37 +6,48 @@ use App\Http\Controllers\Controller;
 use App\Models\CommentsModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Mail;
 
 class CommentsController extends Controller
 {
     //
     public function store(Request $request)
     {
-    	$request->validate([
-            'content'=>'required',
+        $request->validate([
+            'content' => 'required',
         ]);
 
         $cmt = new CommentsModel;
         $id = Auth::id();
-        $cmt -> product_id = $request -> product_id;
-        $cmt -> content = $request -> content;
-        $cmt -> user_id = $id;
+        $cmt->product_id = $request->product_id;
+        $cmt->content = $request->content;
+        $cmt->user_id = $id;
+        $user = Auth::user();
+        Mail::send('SendEmail.comment', compact('user', 'cmt'), function ($email) use ($user) {
+            $email->subject('Thông báo bình luận');
+            $email->to($user['email'], $user['name']);
+        });
         $cmt->save();
         return back();
     }
 
     public function replies(Request $request)
     {
-    	$request->validate([
-            'content'=>'required',
+        $request->validate([
+            'content' => 'required',
         ]);
 
         $cmt = new CommentsModel;
         $id = Auth::id();
-        $cmt -> product_id = $request -> product_id;
-        $cmt -> parent_id = $request -> parent_id;
-        $cmt -> content = $request -> content;
-        $cmt -> user_id = $id;
+        $cmt->product_id = $request->product_id;
+        $cmt->parent_id = $request->parent_id;
+        $cmt->content = $request->content;
+        $cmt->user_id = $id;
+        $user = Auth::user();
+        Mail::send('SendEmail.comment', compact('user', 'cmt'), function ($email) use ($user) {
+            $email->subject('Thông báo bình luận');
+            $email->to($user['email'], $user['name']);
+        });
         $cmt->save();
         return back();
     }
